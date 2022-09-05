be952de5520886dbeddc786848788de141d76554
Vit <vit_2282@mail.ru>
2022-09-05 11:38:57 +0700

    Первое задание

---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿        // Первая задача
+
+// int a = 5;
+// int b = 7;
+
+// if (a > b){
+//     System.Console.WriteLine("max = 5");
+// }
+// else{
+//     if (b > a){
+//         System.Console.WriteLine("max = 7");
+//     }
+//     else{
+//         System.Console.WriteLine("a = b");
+//     }
+// }
+
+
+// int a = 2;
+// int b = 10;
+
+// if (a > b){
+//     System.Console.WriteLine("max = 2");
+// }
+// else{
+//     if (b > a){
+//         System.Console.WriteLine("max = 10");
+//     }
+//     else{
+//         System.Console.WriteLine("a = b");
+//     }
+// }
+
+
+
+// int a = -9;
+// int b = -3;
+
+// if (a > b){
+//     System.Console.WriteLine("max = -9");
+// }
+// else{
+//     if (b > a){
+//         System.Console.WriteLine("max = -3");
+//     }
+//     else{
+//         System.Console.WriteLine("a = b");
+//     }
+// }
+
+
+        // Задача 2
+
+// int a = 2;
+// int b = 3;
+// int c = 7;
+
+// int max =a;
+
+// if (max < b){
+//     max = b;
+// }
+
+// if (max < c){
+//     max = c;
+// }
+// System.Console.WriteLine(max);
+
+
+
+// int a = 44;
+// int b = 5;
+// int c = 78;
+
+// int max =a;
+
+// if (max < b){
+//     max = b;
+// }
+
+// if (max < c){
+//     max = c;
+// }
+// System.Console.WriteLine(max);
+
+
+// int a = 22;
+// int b = 3;
+// int c = 9;
+
+// int max =a;
+
+// if (max < b){
+//     max = b;
+// }
+
+// if (max < c){
+//     max = c;
+// }
+// System.Console.WriteLine(max);
+
+
+
+
+        // Задача 3
+
+
+// int a = 4;
+// if (a % 2 == 0){
+//     System.Console.WriteLine("это чётное число");
+// }
+// else {
+//     System.Console.WriteLine("это не чётное число");
+// }
+
+
+// int a = -3;
+// if (a % 2 == 0){
+//     System.Console.WriteLine("это чётное число");
+// }
+// else {
+//     System.Console.WriteLine("это не чётное число");
+// }
+
+
+
+// int a = 7;
+// if (a % 2 == 0){
+//     System.Console.WriteLine("это чётное число");
+// }
+// else {
+//     System.Console.WriteLine("это не чётное число");
+// }
+
+
+
+
+        // Задача 4
+
+        // int n = 5;
+        // int i =1;
+
+        // while (i<= n){
+        //     if (i % 2 == 0){
+        //         System.Console.WriteLine(i);
+        //     }
+        //     i++;
+        // }
+
+
+        //     int n = 8;
+        // int i =1;
+
+        // while (i<= n){
+        //     if (i % 2 == 0){
+        //         System.Console.WriteLine(i);
+        //     }
+        //     i++;
+        // }
