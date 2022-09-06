9772d3f907785ef662680e1a01577d2d6bd19013
Виталий Михалев <111574045+vit42rus@users.noreply.github.com>
2022-09-06 08:37:53 +0700

    задание 2

--- Program.cs
+++ Program.cs
@@ -1,159 +1,150 @@
-﻿        // Первая задача
+﻿
 
-// int a = 5;
-// int b = 7;
+            // Задача 1
 
-// if (a > b){
-//     System.Console.WriteLine("max = 5");
+Console.Write("Введите трехзначное число :");
+
+ int n = Convert.ToInt32(Console.ReadLine());
+
+int a1 = n / 10;
+
+
+int a2 = a1 % 10;
+
+
+ System.Console.WriteLine(a2);
+
+
+ 
+
+//  int n = 456;
+// int a1 = n / 10;
+// int a2 = a1 % 10;
+
+//  System.Console.WriteLine(a2);
+
+
+ 
+//  int n = 782;
+// int a1 = n / 10;
+// int a2 = a1 % 10;
+
+//  System.Console.WriteLine(a2);
+ 
+
+ 
+//  int n = 918;
+// int a1 = n / 10;
+// int a2 = a1 % 10;
+
+//  System.Console.WriteLine(a2);
+
+
+
+            // Задача 2
+
+
+
+//    Console.Write("Введите трехзначное число :");
+
+//   int n = Convert.ToInt32(Console.ReadLine());
+//   if (n <= 99) {
+//   System.Console.WriteLine("Нет третий цифры");
+//   }
+
+//   else if (n >=1000) {
+//   int a2 = n / 100 ;
+//   int a3 = a2 % 10;
+//   System.Console.WriteLine(a3);
+//   }
+//   else {
+                                    
+//   int a1 = n % 10;
+//   System.Console.WriteLine(a1);
+//   }
+
+
+
+
+ 
+//  int n = 645;
+
+
+// if (n <=99) {
+//      System.Console.WriteLine("Нет третий цифры");
+    
 // }
-// else{
-//     if (b > a){
-//         System.Console.WriteLine("max = 7");
-//     }
-//     else{
-//         System.Console.WriteLine("a = b");
-//     }
+//  else if (n >=1000){
+//      int a2 = n / 100;
+// int a3 = a2 % 10;
+//  System.Console.WriteLine(a3);
 // }
+//        else  {
+        
+//        int a1 = n % 10;
+//        System.Console.WriteLine(a1);
+//         }
+    
 
 
-// int a = 2;
-// int b = 10;
+//  int n = 78;
 
-// if (a > b){
-//     System.Console.WriteLine("max = 2");
-// }
-// else{
-//     if (b > a){
-//         System.Console.WriteLine("max = 10");
-//     }
-//     else{
-//         System.Console.WriteLine("a = b");
-//     }
-// }
-
-
-
-// int a = -9;
-// int b = -3;
-
-// if (a > b){
-//     System.Console.WriteLine("max = -9");
-// }
-// else{
-//     if (b > a){
-//         System.Console.WriteLine("max = -3");
-//     }
-//     else{
-//         System.Console.WriteLine("a = b");
-//     }
-// }
-
-
-        // Задача 2
-
-// int a = 2;
-// int b = 3;
-// int c = 7;
-
-// int max =a;
-
-// if (max < b){
-//     max = b;
-// }
-
-// if (max < c){
-//     max = c;
-// }
-// System.Console.WriteLine(max);
-
-
-
-// int a = 44;
-// int b = 5;
-// int c = 78;
-
-// int max =a;
-
-// if (max < b){
-//     max = b;
-// }
-
-// if (max < c){
-//     max = c;
-// }
-// System.Console.WriteLine(max);
-
-
-// int a = 22;
-// int b = 3;
-// int c = 9;
-
-// int max =a;
-
-// if (max < b){
-//     max = b;
-// }
-
-// if (max < c){
-//     max = c;
-// }
-// System.Console.WriteLine(max);
-
-
-
-
-        // Задача 3
-
-
-// int a = 4;
-// if (a % 2 == 0){
-//     System.Console.WriteLine("это чётное число");
-// }
-// else {
-//     System.Console.WriteLine("это не чётное число");
+// if (n <99) {
+//      System.Console.WriteLine("Нет третий цифры");
+    
 // }
 
 
-// int a = -3;
-// if (a % 2 == 0){
-//     System.Console.WriteLine("это чётное число");
+// else if (n >=1000){
+//      int a2 = n / 100;
+// int a3 = a2 % 10;
+//  System.Console.WriteLine(a3);
 // }
-// else {
-//     System.Console.WriteLine("это не чётное число");
+//        else  {
+        
+//        int a1 = n % 10;
+//        System.Console.WriteLine(a1);
+//         }
+    
+
+//      int n = 32679;
+
+// if (n <99) {
+//      System.Console.WriteLine("Нет третий цифры");
+    
+// }
+// else if (n >=1000){
+//      int a2 = n / 100;
+// int a3 = a2 % 10;
+//  System.Console.WriteLine(a3);
+// }
+//        else  {
+        
+//        int a1 = n % 10;
+//        System.Console.WriteLine(a1);
+//         }
+    
+
+
+
+
+                // Задача 3
+
+
+// Console.Write("Введите номер дня недели :");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+
+// if (day == 6 || day == 7){
+//   Console.WriteLine("Да это выходной");
+// }
+// else if (day < 1 || day > 7){
+//     Console.WriteLine("Это не день недели");
+
+// }
+// else if (day < 5 || day > 1){
+//     Console.WriteLine("Нет это не выходной");
 // }
 
 
 
-// int a = 7;
-// if (a % 2 == 0){
-//     System.Console.WriteLine("это чётное число");
-// }
-// else {
-//     System.Console.WriteLine("это не чётное число");
-// }
-
-
-
-
-        // Задача 4
-
-        // int n = 5;
-        // int i =1;
-
-        // while (i<= n){
-        //     if (i % 2 == 0){
-        //         System.Console.WriteLine(i);
-        //     }
-        //     i++;
-        // }
-
-
-        //     int n = 8;
-        // int i =1;
-
-        // while (i<= n){
-        //     if (i % 2 == 0){
-        //         System.Console.WriteLine(i);
-        //     }
-        //     i++;
-        // }
